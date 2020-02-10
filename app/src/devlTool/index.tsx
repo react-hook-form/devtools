2c53369ceacd398bcf6208fedd8231059dc999ad
bluebill1049 <bluebill1049@hotmail.com>
2020-02-10 14:43:16 +1100

    add toggle functionality

--- app/src/devlTool/index.tsx
+++ app/src/devlTool/index.tsx
@@ -1,56 +1,74 @@
 import * as React from 'react';
 import { Control } from 'react-hook-form';
 import Header from './header';
-import Content from './panel';
+import Panel from './panel';
 import colors from './colors';
+import Logo from './logo';
 
 export default ({ control }: { control: Control }) => {
+  const [visible, setVisible] = React.useState(false);
+
   return (
     <>
-      <div
-        style={{
-          position: 'fixed',
-          height: '100vh',
-          width: 250,
-          zIndex: 99999,
-          background: colors.primary,
-          top: 0,
-          right: 0,
-          display: 'grid',
-          textAlign: 'left',
-          color: 'white',
-          fontSize: 14,
-          gridTemplateRows: '40px auto',
-        }}
-      >
-        <Header />
-        <Content control={control} />
-      </div>
-      <div
-        style={{
-          position: 'fixed',
-          height: '100vh',
-          width: 250,
-          zIndex: 99998,
-          background: 'black',
-          top: 7,
-          right: 7,
-          boxShadow: 'rgba(15, 15, 29, 0.4) -3px 3px 6px 0px',
-        }}
-      />
-      <span
-        style={{
-          width: 0,
-          height: 0,
-          position: 'fixed',
-          zIndex: 99998,
-          borderTop: '10px solid transparent',
-          borderBottom: '10px solid transparent',
-          borderRight: `10px solid black`,
-          right: 247,
-          top: -3,
-        }}
-      />
+      {visible ? (
+        <>
+          <div
+            style={{
+              position: 'fixed',
+              height: '100vh',
+              width: 250,
+              zIndex: 99999,
+              background: colors.primary,
+              top: 0,
+              right: 0,
+              display: 'grid',
+              textAlign: 'left',
+              color: 'white',
+              fontSize: 14,
+              gridTemplateRows: '40px auto',
+            }}
+          >
+            <Header setVisible={setVisible} />
+            <Panel control={control} />
+          </div>
+          <div
+            style={{
+              position: 'fixed',
+              height: '100vh',
+              width: 250,
+              zIndex: 99998,
+              background: 'black',
+              top: 7,
+              right: 7,
+              boxShadow: 'rgba(15, 15, 29, 0.4) -3px 3px 6px 0px',
+            }}
+          />
+          <span
+            style={{
+              width: 0,
+              height: 0,
+              position: 'fixed',
+              zIndex: 99998,
+              borderTop: '10px solid transparent',
+              borderBottom: '10px solid transparent',
+              borderRight: `10px solid black`,
+              right: 247,
+              top: -3,
+            }}
+          />
+        </>
+      ) : (
+        <div
+          style={{
+            position: 'fixed',
+            zIndex: 99999,
+            top: 0,
+            right: 0,
+          }}
+        >
+          <Logo setVisible={setVisible} />
+        </div>
+      )}
     </>
   );
 };
--- app/src/devlTool/logo.tsx
+++ app/src/devlTool/logo.tsx
@@ -1,18 +1,14 @@
 import colors from './colors';
 import * as React from 'react';
 
-type Props = {
+const Logo = ({
+  style,
+  setVisible,
+}: {
+  style?: Record<string, any>;
   setVisible: (visible: boolean) => void;
-};
-
-export default ({ setVisible }: Props) => (
-  <header
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      paddingLeft: 10,
-    }}
-  >
+}) => {
+  return (
     <svg
       fill="white"
       viewBox="0 0 100 100"
@@ -20,8 +16,13 @@ export default ({ setVisible }: Props) => (
         height: 12,
         padding: 3,
         borderRadius: 2,
-        marginRight: 10,
         background: colors.lightPink,
+        ...style,
+      }}
+      onClick={() => {
+        if (setVisible) {
+          setVisible(true);
+        }
       }}
     >
       <path d="M73.56,13.32H58.14a8.54,8.54,0,0,0-16.27,0H26.44a11,11,0,0,0-11,11V81.63a11,11,0,0,0,11,11H73.56a11,11,0,0,0,11-11V24.32A11,11,0,0,0,73.56,13.32Zm-30.92,2a1,1,0,0,0,1-.79,6.54,6.54,0,0,1,12.78,0,1,1,0,0,0,1,.79h5.38v6.55a3,3,0,0,1-3,3H40.25a3,3,0,0,1-3-3V15.32ZM82.56,81.63a9,9,0,0,1-9,9H26.44a9,9,0,0,1-9-9V24.32a9,9,0,0,1,9-9h8.81v6.55a5,5,0,0,0,5,5h19.5a5,5,0,0,0,5-5V15.32h8.81a9,9,0,0,1,9,9Z" />
@@ -36,21 +37,7 @@ export default ({ setVisible }: Props) => (
       />
       <path d="M71.1,69.49H53.45a1,1,0,1,0,0,2H71.1a1,1,0,0,0,0-2Z" />
     </svg>
-    <p>Developer Tool</p>
-    <button
-      onClick={() => setVisible(false)}
-      style={{
-        appearance: 'none',
-        background: 'none',
-        border: 'none',
-        color: 'white',
-        fontSize: 14,
-        fontWeight: 'bold',
-        marginRight: 4,
-        marginLeft: 'auto',
-      }}
-    >
-      ✕
-    </button>
-  </header>
-);
+  );
+};
+
+export default Logo;
