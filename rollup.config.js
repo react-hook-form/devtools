a956d17ab92c07e96a0170c4ab54611150acf332
Kotaro Sugawara <kotarella1110@gmail.com>
2020-06-27 14:22:13 +0900

    support umd build (#35)
    * support umd build
    
    * change umd name

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,58 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.tsx',
+    output: {
+      name: 'ReactHookFormDevTools',
+      file: pkg.unpkg,
+      format: 'umd',
       sourcemap: true,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'react-hook-form': 'ReactHookForm',
+      },
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    json(),
-    typescript({
-      clean: true,
-    }),
-    commonjs(),
-    resolve(),
-    sourcemaps(),
-  ],
-};
+    plugins: [
+      external(),
+      json(),
+      typescript({
+        clean: true,
+      }),
+      commonjs(),
+      resolve(),
+      sourcemaps(),
+      terser(),
+    ],
+  },
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      json(),
+      typescript({
+        clean: true,
+      }),
+      commonjs(),
+      resolve(),
+      sourcemaps(),
+    ],
+  },
+];
