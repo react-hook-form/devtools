e8529c10f7254e89b1ce19d11c32f020e0494ab2
Justin Sun <i@jsun969.cn>
2022-07-09 20:15:07 +0800

    refactor: make `toFlat` as utility (#152)

--- src/extension/utils.ts
+++ src/extension/utils.ts
@@ -1,6 +1,20 @@
+import _ from 'lodash';
+
 export function proxyToObject<T extends Record<string, any>>(proxy: T) {
   return Reflect.ownKeys(proxy).reduce((perv, key) => {
     perv[key as keyof T] = proxy[key as keyof T];
     return perv;
   }, {} as T);
+}
+
+export function nestToFlat<V>(
+  flatKeys: string[],
+  obj: object,
+  defaultValue?: V,
+) {
+  return flatKeys.reduce((perv, name) => {
+    // nested field may be `undefined`
+    perv[name] = _.get(obj, name) || defaultValue;
+    return perv;
+  }, {} as Record<string, V>);
 }
