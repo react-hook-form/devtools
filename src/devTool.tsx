0af2e202c58898feb649c38f2a75f447c2393e02
Justin Sun <i@jsun969.cn>
2022-07-09 17:51:02 +0800

    Chrome extension support (#150)
    * chore(ts): upgrade `target` version to `es6`
    
    * feat: add chrome extension support
    
    * ci: upgrade node version to 16
    
    * refactor: optimize getting control

--- src/devTool.tsx
+++ src/devTool.tsx
@@ -1,7 +1,8 @@
+import { createStore, StateMachineProvider } from 'little-state-machine';
 import * as React from 'react';
-import { StateMachineProvider, createStore } from 'little-state-machine';
 import { Control, FieldValues, useFormContext } from 'react-hook-form';
 import { DevToolUI } from './devToolUI';
+import { useExportControlToExtension } from './extension/useExportControlToExtension';
 import type { PLACEMENT } from './position';
 
 if (typeof window !== 'undefined') {
@@ -25,10 +26,17 @@ export const DevTool = <T extends FieldValues>(props?: {
 }) => {
   const methods = useFormContext();
 
+  const { isExtensionEnabled } = useExportControlToExtension(
+    props?.control ?? methods.control,
+  );
+  if (isExtensionEnabled) {
+    return null;
+  }
+
   return (
     <StateMachineProvider>
       <DevToolUI
-        control={(props && props.control) || methods.control}
+        control={props?.control ?? methods.control}
         placement={props?.placement}
       />
     </StateMachineProvider>
