852f6a37e01da0fa351af8bcf0000febeebf6e50
Justin Sun <i@jsun969.cn>
2022-07-14 17:29:16 +0800

    feat: remove nextjs example

--- nextjs-example/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import Form1 from '../components/forms/Form1';
-
-const Home: NextPage = () => {
-  return <Form1 />;
-};
-
-export default Home;
